101253f08c03875526b269876d68d7f424c6ba34
Christian Hau <christian@c8121.de>
2023-03-21 17:16:08 +0100

    Added filter/cat-mime-message.sh

---
+++ filter/cat-mime-message.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASE=$(realpath "$(dirname "$0")")
+PARSER_COMMAND="$BASE/../bin/mailparser"
+CONVERT_COMMAND="$BASE/convert-to-text.sh"
+
+file="$1"
+
+if [ "$file" == "" ]; then
+  echo "Usage: $0 <filename>" >&2
+  exit
+fi
+
+if [ ! -f "$file" ]; then
+  echo "File not found" >&2
+  exit
+fi
+
+temp_dir=$(mktemp -d)
+
+$PARSER_COMMAND "$temp_dir" <"$file"
+find "$temp_dir" -type f -print0 | while IFS= read -r -d '' content_file; do
+
+  #ls -l "$content_file"
+  $CONVERT_COMMAND "$content_file"
+
+done
+
+rm -r "$temp_dir"
