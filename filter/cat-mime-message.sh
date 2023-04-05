0acc48148e496cca1503c70e46058c7431a2b022
Christian Hau <christian@c8121.de>
2023-04-05 17:11:36 +0200

    Converting mime messages: Temp dir can be selected

--- filter/cat-mime-message.sh
+++ filter/cat-mime-message.sh
@@ -4,19 +4,40 @@ BASE=$(realpath "$(dirname "$0")")
 PARSER_COMMAND="$BASE/../bin/mailparser"
 CONVERT_COMMAND="$BASE/convert-to-text.sh"
 
+function usage_message() {
+  echo "Usage: $0 <filename> [temp dir]" >&2
+}
+
 file="$1"
 
 if [ "$file" == "" ]; then
-  echo "Usage: $0 <filename>" >&2
+  usage_message
   exit
 fi
 
 if [ ! -f "$file" ]; then
   echo "File not found" >&2
+  usage_message
   exit
 fi
 
-temp_dir=$(mktemp -d)
+temp_dir="$2"
+if [ "$temp_dir" == "" ]; then
+  temp_dir=$(mktemp -d)
+elif [ -f "$temp_dir" ]; then
+  echo "Temp dir is a file" >&2
+  usage_message
+  exit
+elif [ ! -d "$temp_dir" ]; then
+  echo "Temp dir does not exist" >&2
+  usage_message
+  exit
+elif [ "$(ls -A "$temp_dir")" ]; then
+  echo "Temp dir must be empty" >&2
+  usage_message
+  exit
+fi
+
 
 $PARSER_COMMAND "$temp_dir" -ignore image/ <"$file" >&2
 find "$temp_dir" -type f -print0 | while IFS= read -r -d '' content_file; do
--- filter/convert-to-text.sh
+++ filter/convert-to-text.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
+function usage_message() {
+  echo "Usage: $0 <filename> [temp dir]" >&2
+}
+
 file="$1"
 
 if [ "$file" == "" ]; then
-  echo "Usage: $0 <filename>" >&2
+  usage_message
   exit
 fi
 
 if [ ! -f "$file" ]; then
   echo "File not found" >&2
+  usage_message
   exit
+fi
+
+temp_dir="$2"
+if [ "$temp_dir" != "" ]; then
+  if [ -f "$temp_dir" ]; then
+    echo "Temp dir is a file" >&2
+    usage_message
+    exit
+  elif [ ! -d "$temp_dir" ]; then
+    echo "Temp dir does not exist" >&2
+    usage_message
+    exit
+  elif [ "$(ls -A "$temp_dir")" ]; then
+    echo "Temp dir must be empty" >&2
+    usage_message
+    exit
+  fi
 fi
 
 mimeType=$(file -b --mime-type "$file")
@@ -41,7 +63,7 @@ text/csv)
 
 message/rfc822)
   cmd="$(dirname "$0")"/cat-mime-message.sh
-  "$cmd" "$file"
+  "$cmd" "$file" "$temp_dir"
   ;;
 
 *)
